a33cbf80f6daa80256b04d5e962c789fd84f31f1
iceboy95 <hqkhor95@gmail.com>
2016-12-18 16:44:42 +0800

    added required formats

--- Assignment/as1/coinCounting.py
+++ Assignment/as1/coinCounting.py
@@ -21,61 +21,72 @@ def coinCount(coinMat, i):
 	#
 	#########################################################################
 	# ADD YOUR CODE BELOW THIS LINE
+	num_pic = 1
 
-	######## Loading #######
-	string_for_image = coinMat + "_" + str(i) + '.jpg'
-	coin_image_gray = cv2.imread(string_for_image, cv2.IMREAD_GRAYSCALE)
-	coin_image = cv2.imread(string_for_image)
-	########################
+	while num_pic <= numImages:
+		######## Loading #######
+		string_for_image = "coin" + "_" + "s" + str(i) + "_" + str(num_pic) + '.jpg'
+		coin_image_gray = cv2.imread(string_for_image, cv2.IMREAD_GRAYSCALE)
+		coin_image = cv2.imread(string_for_image)
+		########################
 
-	######## Thresholding #########
-	retOtsu, threshOtsu = cv2.threshold(coin_image_gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
-	retOtsu, threshOtsu_noInv = cv2.threshold(coin_image_gray, 0, 255, cv2.THRESH_OTSU) #for concatenation
-	#############################
+		######## Thresholding #########
+		retOtsu, threshOtsu = cv2.threshold(coin_image_gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)
+		retOtsu, threshOtsu_noInv = cv2.threshold(coin_image_gray, 0, 255, cv2.THRESH_OTSU) #for concatenation
+		#############################
 
-	######## Morphological Ops #########
-	kernel = np.ones((6,6),np.uint8)
-	morphOpen = cv2.morphologyEx(threshOtsu, cv2.MORPH_OPEN, kernel)
-	morph_to_be_parsed = morphOpen
-	####################################
-	
-	######## Edge Detection(Currently Not Used) ###########
-	canny_edge_detector = cv2.Canny(morphOpen, retOtsu, 255)
-	###################################
+		######## Morphological Ops #########
+		kernel = np.ones((6,6),np.uint8)
+		morphOpen = cv2.morphologyEx(threshOtsu, cv2.MORPH_OPEN, kernel)
+		morph_to_be_parsed = morphOpen
+		####################################
+		
+		######## Edge Detection(Currently Not Used) ###########
+		canny_edge_detector = cv2.Canny(morphOpen, retOtsu, 255)
+		###################################
 
-	########### Find Contours ###########
-	contours_image, contours, hierarchy = cv2.findContours(morph_to_be_parsed
-		, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	#####################################
+		########### Find Contours ###########
+		contours_image, contours, hierarchy = cv2.findContours(morph_to_be_parsed
+			, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		#####################################
 
-	########### Find Rectangles for edges #############
-	for cnt in contours:
-		x,y,w,h = cv2.boundingRect(cnt)
-		###### Remove possible noises #######
-		print(str(x) + " " + str(y) + " " + str(x+w) + " " + str(y+h))
-		if w >= 20 and h >= 30:   # the minimum width to be considered a coin
-			cv2.rectangle(coin_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-	###################################################
+		########### Find Rectangles for edges #############
+		for cnt in contours:
+			x,y,w,h = cv2.boundingRect(cnt)
+			###### Remove possible noises #######
+			print(str(x) + " " + str(y) + " " + str(x+w) + " " + str(y+h))
+			if w >= 20 and h >= 30:   # the minimum width to be considered a coin
+				cv2.rectangle(coin_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		###################################################
 
-	plt.subplot(231), plt.imshow(threshOtsu, cmap = 'gray')
-	plt.title('Thresholded Image For Otsu'), plt.xticks([]), plt.yticks([])
-	# plt.subplot(232), plt.hist(coin_image.ravel(), 256, [0, 256])
-	plt.subplot(232), plt.imshow(morphOpen, cmap = 'gray')
-	plt.title('morphOpen'), plt.xticks([]), plt.yticks([])
-	plt.subplot(233), plt.imshow(canny_edge_detector, cmap = 'gray')
-	plt.title('Canny Edge Detector'), plt.xticks([]), plt.yticks([])
-	# plt.subplot(234), plt.imshow(contours_image, cmap = 'gray')
-	plt.subplot(234), plt.imshow(coin_image, cmap = 'gray')
-	plt.title('Bounding Box'), plt.xticks([]), plt.yticks([])
-	plt.subplot(235), plt.imshow(contours_image, cmap = 'gray')
-	plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
-	plt.show()
-	#############################
-
+		plt.figure(figsize = (10,10))
+		plt.subplot(231), plt.imshow(threshOtsu, cmap = 'gray')
+		plt.title('Thresholded Image For Otsu'), plt.xticks([]), plt.yticks([])
+		# plt.subplot(232), plt.hist(coin_image.ravel(), 256, [0, 256])
+		plt.subplot(232), plt.imshow(morphOpen, cmap = 'gray')
+		plt.title('morphOpen'), plt.xticks([]), plt.yticks([])
+		plt.subplot(233), plt.imshow(canny_edge_detector, cmap = 'gray')
+		plt.title('Canny Edge Detector'), plt.xticks([]), plt.yticks([])
+		# plt.subplot(234), plt.imshow(contours_image, cmap = 'gray')
+		plt.subplot(234), plt.imshow(coin_image, cmap = 'gray')
+		plt.title('Bounding Box'), plt.xticks([]), plt.yticks([])
+		plt.subplot(235), plt.imshow(contours_image, cmap = 'gray')
+		plt.title('Contour Image'), plt.xticks([]), plt.yticks([])
+		plt.show()
+		#############################
+		num_pic += 1
 
 	# END OF YOUR CODE
 	#########################################################################
 	# return ans
 
-coinCount("coin_s1", 4)
+standard_picture_size = cv2.imread("coin_s1_2.jpg")
+standard_picture_size = standard_picture_size.shape
+row = standard_picture_size[0]
+col = standard_picture_size[1]
+channels = standard_picture_size[2]
+numImages = 10
+input_image = row * col * channels * numImages
+
+coinCount(input_image, 1)
 
